38fd2bc846862ea26bcce373e94f4bdb493653ce
polina-tsvilodub <polina.tsvilodub@gmail.com>
2024-12-27 15:40:12 -0800

    update completion code

--- experiments/goal_sampling/src/magpie.config.js
+++ experiments/goal_sampling/src/magpie.config.js
@@ -3,7 +3,7 @@ export default {
   serverUrl: 'https://magpie-cogsciprag.fly.dev',
   socketUrl: 'wss://magpie-cogsciprag.fly.dev/socket',
   // this will be used in prolific mode
-  completionUrl: 'https://app.prolific.com/submissions/complete?cc=C4SMX8WN',
+  completionUrl: 'https://app.prolific.com/submissions/complete?cc=CRQTR8R7',
   contactEmail: 'polina.tsvilodub@uni-tuebingen.de',
   // Either 'debug', 'directLink' or 'prolific'
   mode: 'prolific',
