27c03db12d81ec0a73ed175ae324f358c11da27b
polina-tsvilodub <polina.tsvilodub@gmail.com>
2025-01-31 01:33:36 +0100

    run full expt for scil

--- experiments/goal_sampling/src/magpie.config.js
+++ experiments/goal_sampling/src/magpie.config.js
@@ -1,5 +1,5 @@
 export default {
-  experimentId: '11',
+  experimentId: '10',
   serverUrl: 'https://magpie-cogsciprag.fly.dev',
   socketUrl: 'wss://magpie-cogsciprag.fly.dev/socket',
   // this will be used in prolific mode
